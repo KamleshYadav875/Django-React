d9dc62b9ee5c0272c3d8ab6c29e001fa442a52ff
Kamlesh Yadav <56463146+KamleshYadav875@users.noreply.github.com>
2020-08-16 09:31:40 +0530

    Add files via upload

---
+++ front-end/build/precache-manifest.65ca84abe965751320921d908068ef3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2787289080e25998518b91a5c394beea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90e458329d9dba725c08",
+    "url": "/static/css/main.c87ca641.chunk.css"
+  },
+  {
+    "revision": "6e07c7e02ff370cc246d",
+    "url": "/static/js/2.6f718e4d.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.6f718e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e458329d9dba725c08",
+    "url": "/static/js/main.4a799a55.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
